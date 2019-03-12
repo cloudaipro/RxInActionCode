6aa5c6191ec584873a96f64c9316a3e20c5d28d7
DESKTOP-IDEA idea <cloudaipro@gmail.com>
2019-03-11 22:42:00 -0700

    chap5

---
+++ chap5/chap5/MagicalPrimeGenerator.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap5
+{
+    class MagicalPrimeGenerator
+    {
+        public int startOff = 100000;
+
+
+        public async Task<IReadOnlyCollection<int>> GenerateAsync(int amount)
+        {
+            return await Task.Run(() => Generate(amount).ToList().AsReadOnly());
+
+        }
+
+        public IEnumerable<int> Generate(int amount)
+        {
+
+            foreach(int prime in GeneratePrime())
+            {
+                yield return prime;
+                if (--amount <= 0)
+                    break;
+            }
+
+        }
+
+        public IEnumerable<int> GeneratePrime()
+        {
+            IList<int> primes = new List<int>();
+            primes.Add(2);
+            yield return 2;
+            int curTest = 3;
+            while (true)
+            {
+                int sqrt = (int)Math.Sqrt(curTest);
+                bool isPrime = true;
+                for (int i = 0; i < primes.Count && primes[i] <= sqrt; i++)
+                    if (curTest % primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                if (isPrime)
+                {
+                    primes.Add(curTest);
+                    yield return curTest;
+                }
+                curTest += 2;
+            }
+
+        }
+
+        //public IEnumerable<int> GenerateAsync(int amount)
+        //{
+        //    amount += startOff;
+        //    IList<int> rtnPrimes = new List<int>();
+        //    IList<int> primes = new List<int>();
+        //    primes.Add(2);
+
+        //    int curTest = 3;
+        //    while(primes.Count < amount)
+        //    {
+        //        int sqrt = (int)Math.Sqrt(curTest);
+        //        bool isPrime = true;
+        //        for (int i = 0; i < primes.Count && primes[i] <= sqrt; i++)
+        //            if (curTest % primes[i] == 0)
+        //            {
+        //                isPrime = false;
+        //                break;
+        //            }
+        //        if (isPrime)
+        //        {
+        //            primes.Add(curTest);
+        //            if (primes.Count > startOff)
+        //            {
+        //                rtnPrimes.Add(curTest);
+        //                Console.WriteLine("GenerateAsync@" + curTest);
+        //                yield return curTest;
+        //            }
+        //        }
+        //        curTest += 2;
+        //    } 
+        //}
+        //public IEnumerable<int> Generate(int amount)
+        //{
+        //    amount += startOff;
+        //    IList<int> rtnPrimes = new List<int>();
+        //    IList<int> primes = new List<int>();
+        //    primes.Add(2);
+
+        //    int curTest = 3;
+        //    while (primes.Count < amount)
+        //    {
+        //        int sqrt = (int)Math.Sqrt(curTest);
+        //        bool isPrime = true;
+        //        for (int i = 0; i < primes.Count && primes[i] <= sqrt; i++)
+        //            if (curTest % primes[i] == 0)
+        //            {
+        //                isPrime = false;
+        //                break;
+        //            }
+        //        if (isPrime)
+        //        {
+        //            primes.Add(curTest);
+        //            if (primes.Count > startOff)
+        //            {
+        //                rtnPrimes.Add(curTest);
+        //            }
+        //        }
+        //        curTest += 2;
+        //    }
+        //    return rtnPrimes;
+        //}
+    }
+}
---
+++ chap5/chap5/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace chap5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int iAmount = Int32.Parse(amount.Text);
+            MagicalPrimeGenerator generator =  new MagicalPrimeGenerator();
+            generator.startOff = Int32.Parse(offset.Text);
+            foreach (int i in generator.Generate(iAmount))
+                Console.WriteLine(DateTime.Now.ToString("mm:ss.fff") + "@" + i.ToString());
+            //generator.Generate(iAmount).ToObservable<int>()
+            //                       .Timestamp()
+            //                       .Subscribe(x => Console.WriteLine(x.ToString()));
+            var primes = await generator.GenerateAsync(iAmount);
+            primes.ToObservable<int>()
+                                        .Timestamp()
+                                        .Subscribe(x => Console.WriteLine(x.ToString()));
+        }
+    }
+}
